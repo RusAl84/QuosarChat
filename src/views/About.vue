07c5c097946d86aaacd2f7515227f6406da25b9d
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-11-15 13:43:05 +0300

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <h1>About</h1>
+
+  </q-page>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-page>
+    <h1>Messenger</h1>
+
+
+    <q-input v-model="text" label="Standard" />
+    <q-btn color="blue"  icon-right="send" label="Отправить" @click="send" />
+    <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+            name="me"
+            avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+            :text="['hey, how are you?']"
+            stamp="7 minutes ago"
+            sent
+            bg-color="amber-7"
+        />
+        <q-chat-message
+            name="Jane"
+            avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+            :text="[
+          'doing fine, how r you?',
+          'I just feel like typing a really, really, REALLY long message to annoy you...'
+        ]"
+            size="6"
+            stamp="4 minutes ago"
+            text-color="white"
+            bg-color="primary"
+        />
+        <q-chat-message
+            name="Jane"
+            avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+            :text="['Did it work?']"
+            stamp="1 minutes ago"
+            size="8"
+            text-color="white"
+            bg-color="primary"
+        />
+      </div>
+    </div>
+
+
+  </q-page>
+</template>
+
+<script>
+// @ is an alias to /src
+import api from "@/api/api"
+
+export default {
+  name: 'Home',
+  // components: {
+  //   HelloWorld
+  // }
+  data: () => {
+
+    return {
+      text: '',
+
+
+    }
+  },
+  methods: {
+    async send() {
+      var mas = await api.sendMessage(this.text)
+      console.log(mas[0])
+      // console.log("dssdds")
+      alert(mas)
+    }
+  }
+}
+</script>
